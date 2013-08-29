bbe61764c0df6017903c3362bedd446437830423
mangtomas <rhedzpirit@yahoo.com>
2013-08-28 21:58:56 -0700

    controller/student.php prepare methods for student inner page

---
+++ application/controllers/student.php
@@ -0,0 +1,49 @@
+<?php
+class student extends crackerjack{
+	public function __construct(){
+		parent::__construct();
+		if(isAdmin()==true){
+			//redirect('xadmin/main');
+			//uncomment redirect if for deployment
+		}
+	}
+
+	public function index(){
+		$data['page_title'] = "Dashboard";
+	}
+	
+	public function account($id = false){
+		$data['page_title'] = "Account";
+		switch($id[0]){
+			case 'information':
+				$data['page_title'] = "Account Information";
+				
+			break;
+			case 'settings':
+				$data['page_title'] = "Account Settings";
+			break;
+			default:
+				echo 'information and settings page';
+			break;
+		
+		}
+	
+	}
+	
+	public function tests($id = false){
+		$data['page_title'] = "Tests";
+		$id = empty($id[0]) ? 'all' : $id[0];
+	
+	}
+	
+	public function assignments($id = false){
+		$data['page_title'] = "Assigments";
+		$id = empty($id[0]) ? 'all' : $id[0];
+	}
+	
+	public function my_gradebook($id =false){
+		$data['page_title'] = "My Gradebook";
+	}
+
+
+}
--- application/helpers/natsession.php
+++ application/helpers/natsession.php
@@ -4,14 +4,14 @@ function islogin(){
 		$mvc =& getInstance();
 			return ($mvc->session->_get('islogin')==true) ?  true : false;
 	}
-}
-
-
+	
 function isAdmin(){
 	$mvc =& getInstance();
 		if(islogin()==true){
 			if($mvc->session->_get('uid')==1){
 				return ($mvc->session->_get('uid')==1) ?  true : false;
 			}
+		}else{
+			redirect('main');
 		}
 }
